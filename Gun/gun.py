bff4f757c645b55a1814c3c36d873defcd6cfbb2
unknown <Koshchei.bv@phystech.edu>
2020-10-27 10:27:56 +0300

    Add gun rendering

--- Gun/gun.py
+++ Gun/gun.py
@@ -1,71 +1,73 @@
 import numpy as np
-import pygame 
-from random import randint, gauss
-
-pygame.init()
-SCREEN_SIZE = (700, 500)
-screen = pygame.display.set_mode(SCREEN_SIZE)
-pygame.display.set_caption("The gun of Khirianov")
-random_color = (randint(0, 255), randint(0, 255), randint(0,255))
+import pygame as pg
+SCREEN_SIZE = (800, 600)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
-class Gun():
-    pass
-
-
-
-class Table():
-    pass
-
+pg.init()
 
 class Ball():
     pass
+class Table():
+    pass
+
+class Gun():
+    pass
+    def __init__(self, coord=[30, SCREEN_SIZE[1]//2]):
+        self.coord = coord
+        self.angle = 0
+
+    def draw(self, screen):
+        end_pos = [self.coord[0] + 20*np.cos(self.angle), self.coord[1] + 20*np.sin(self.angle)]
+        pg.draw.line(screen, RED, self.coord, end_pos, 5)
+
+    def strike(self):
+        pass
+
+    def set_angle(self, mouse_pos):
+        self.angle = np.arctan2(mouse_pos[1] - self.coord[1], 
+                                mouse_pos[0] - self.coord[0])
 
 
 class Target():
     pass
-
-
-
-
 class Manager():
     def __init__(self):
         self.gun = Gun()
-
-
-
         self.table = Table()
-
+    
     def process(self, events, screen):
         done = self.handle_events(events)
-
         self.draw(screen)
-
         return done
-
 
     def draw(self, screen):
         screen.fill(BLACK)
+        self.gun.draw(screen)
 
     def handle_events(self, events):
         done = False
         for event in events:
-            if event.type == pygame.QUIT:
+            if event.type == pg.QUIT:
                 done = True
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.gun.coord[1] -= 5
+                elif event.key == pg.K_DOWN:
+                    self.gun.coord[1] += 5
+
+        if pg.mouse.get_focused():
+            mouse_pos = pg.mouse.get_pos()
+            self.gun.set_angle(mouse_pos)
+
         return done
 
-done = False
-clock = pygame.time.Clock()
-
+screen = pg.display.set_mode(SCREEN_SIZE)
+pg.display.set_caption("The gun of Khiryanov")
+clock = pg.time.Clock()
 mgr = Manager()
-
+done = False
 while not done:
     clock.tick(15)
-
-
-    done = mgr.process(pygame.event.get(), screen)
-
-    pygame.display.flip()
-
-
-pygame.quit()
+    done = mgr.process(pg.event.get(), screen)
+    pg.display.flip()
