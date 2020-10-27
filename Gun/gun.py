b034a40683ba19e1b52ab761940dd262b230f34c
unknown <Koshchei.bv@phystech.edu>
2020-10-27 10:32:17 +0300

    Complete ball class, add it's test

--- Gun/gun.py
+++ Gun/gun.py
@@ -1,49 +1,91 @@
-import numpy as np
 import pygame as pg
+import numpy as np
+from random import randint
+
 SCREEN_SIZE = (800, 600)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 pg.init()
 
+
 class Ball():
     pass
+    def __init__(self, coord, vel, rad=15, color=None):
+        if color == None:
+            color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.color = color
+        self.coord = coord
+        self.vel = vel
+        self.rad = rad
+
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, self.coord, self.rad)
+
+    def move(self, t_step=1.):
+        for i in range(2):
+            self.coord[i] += int(self.vel[i] * t_step)
+        self.check_walls()
+
+    def check_walls(self):
+        n = [[1, 0], [0, 1]]
+        for i in range(2):
+            if self.coord[i] < self.rad:
+                self.coord[i] = self.rad
+                self.flip_vel(n[i])
+            elif self.coord[i] > SCREEN_SIZE[i] - self.rad:
+                self.coord[i] = SCREEN_SIZE[i] - self.rad
+                self.flip_vel(n[i])
+
+    def flip_vel(self, axis, coef_perp=1., coef_par=1.):
+        vel = np.array(self.vel)
+        n = np.array(axis)
+        n = n / np.linalg.norm(n)
+        vel_perp = vel.dot(n) * n
+        vel_par = vel - vel_perp
+        ans = -vel_perp * coef_perp + vel_par * coef_par
+        print(vel, ans)
+        self.vel = ans.astype(np.int).tolist()
+
+
 class Table():
     pass
-
 class Gun():
-    pass
     def __init__(self, coord=[30, SCREEN_SIZE[1]//2]):
         self.coord = coord
         self.angle = 0
-
     def draw(self, screen):
         end_pos = [self.coord[0] + 20*np.cos(self.angle), self.coord[1] + 20*np.sin(self.angle)]
         pg.draw.line(screen, RED, self.coord, end_pos, 5)
-
     def strike(self):
         pass
-
     def set_angle(self, mouse_pos):
         self.angle = np.arctan2(mouse_pos[1] - self.coord[1], 
                                 mouse_pos[0] - self.coord[0])
-
-
 class Target():
     pass
 class Manager():
     def __init__(self):
         self.gun = Gun()
         self.table = Table()
-    
+        self.balls = []
+        self.balls.append(Ball([100, 100], [10, 20]))
+
     def process(self, events, screen):
         done = self.handle_events(events)
+        self.move()
         self.draw(screen)
         return done
 
     def draw(self, screen):
         screen.fill(BLACK)
+        for ball in self.balls:
+            ball.draw(screen)
         self.gun.draw(screen)
+
+    def move(self):
+        for ball in self.balls:
+            ball.move()
 
     def handle_events(self, events):
         done = False
@@ -55,13 +97,11 @@ class Manager():
                     self.gun.coord[1] -= 5
                 elif event.key == pg.K_DOWN:
                     self.gun.coord[1] += 5
-
+        
         if pg.mouse.get_focused():
             mouse_pos = pg.mouse.get_pos()
             self.gun.set_angle(mouse_pos)
-
         return done
-
 screen = pg.display.set_mode(SCREEN_SIZE)
 pg.display.set_caption("The gun of Khiryanov")
 clock = pg.time.Clock()
