512f4b00cd0064dab86c936d222506aa2ac36c45
unknown <Koshchei.bv@phystech.edu>
2020-11-05 20:59:07 +0300

    Add docstringes

--- GUN/gun.py
+++ GUN/gun.py
@@ -7,7 +7,13 @@ RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 pg.init()
 class Ball():
+    """
+    Creates balls, manages their movement, collisions, rendering.
+    """
     def __init__(self, coord, vel, rad=20, color=None):
+        """
+        Creates a ball with given initial conditions.
+        """
         if color == None:
             color = (randint(0, 255), randint(0, 255), randint(0, 255))
         self.color = color
@@ -17,9 +23,15 @@ class Ball():
         self.is_alive = True
 
     def draw(self, screen):
+        """
+        Draws the ball on the screen.
+        """
         pg.draw.circle(screen, self.color, self.coord, self.rad)
 
     def move(self, t_step=1., g=2.):
+        """
+        Moves the ball. Velocity of the ball is also changed due to gravity.
+        """
         self.vel[1] += int(g * t_step)
         for i in range(2):
             self.coord[i] += int(self.vel[i] * t_step)
@@ -28,6 +40,9 @@ class Ball():
                self.is_alive = False
 
     def check_walls(self):
+        """
+        Checks if the ball has collided with walls and calls flip_vel method to change its velocity if it did.
+        """
         n = [[1, 0], [0, 1]]
         for i in range(2):
             if self.coord[i] < self.rad:
@@ -38,6 +53,9 @@ class Ball():
                 self.flip_vel(n[i], 0.8, 0.9)
 
     def flip_vel(self, axis, coef_perp=1., coef_par=1.):
+        """
+        Changes the velocity of the ball as if it collided inelastically with a wall with normal vector "axis".
+        """
         vel = np.array(self.vel)
         n = np.array(axis)
         n = n / np.linalg.norm(n)
@@ -46,7 +64,9 @@ class Ball():
         ans = -vel_perp * coef_perp + vel_par * coef_par
         self.vel = ans.astype(np.int).tolist() 
 class Table():
-   
+    """
+    Manages counting of points and showing them to th player.
+    """
     def __init__(self, shoots_in_trgt=0, shoots=0):
         self.shoots_in_trgt = shoots_in_trgt
         self.shoots = shoots
@@ -64,8 +84,13 @@ class Table():
         for i in range(3):
             screen.blit(score_surf[i], [10, 10 + 30*i])    
 class Gun():
-    def __init__(self, coord=[30, SCREEN_SIZE[1]//2], 
-                 min_pow=10, max_pow=60):
+    """
+    Creates a gun, manages its movement, shooting, aiming and rendering.
+    """
+    def __init__(self, coord=[30, SCREEN_SIZE[1]//2], min_pow=10, max_pow=60):
+        """
+        Creates a gun with given initial conditions.
+        """
         self.coord = coord
         self.angle = 0
         self.min_pow = min_pow
@@ -73,23 +98,41 @@ class Gun():
         self.power = min_pow
         self.active = False
     def draw(self, screen):
+        """
+        Draws a gun on the screen.
+        """
         end_pos = [self.coord[0] + self.power*np.cos(self.angle), 
                    self.coord[1] + self.power*np.sin(self.angle)]
         pg.draw.line(screen, RED, self.coord, end_pos, 10)
     def strike(self):
+        """
+        Creates a ball. Velocity of the ball depends on where the gun is pointing and how much power it has.
+        """
         vel = [int(self.power * np.cos(self.angle)), int(self.power * np.sin(self.angle))]
         self.active = False
         self.power = self.min_pow
         return Ball(list(self.coord), vel)
         
     def move(self):
+        """
+        Increases the gun's power.
+        """
         if self.active and self.power < self.max_pow:
             self.power += 2
     def set_angle(self, mouse_pos):
+        """
+        Changes the angle of the gun.
+        """
         self.angle = np.arctan2(mouse_pos[1] - self.coord[1], 
                                 mouse_pos[0] - self.coord[0])
 class Target():
+    """
+    Creates targets, manages their collisions with balls, rendering.
+    """
     def __init__(self, coord = None, color = None, rad = 30):
+        """
+        Creates a target with given initial conditions.
+        """
         if color == None:
             color = (randint(0, 255), randint(0, 255), randint(0, 255))
         if coord == None:
@@ -103,16 +146,28 @@ class Target():
         self.rad = rad
     
     def check_collision(self, ball):
+        """
+        Checks if the ball collided with the target.
+        """
         dist = sum([(self.coord[i] - ball.coord[i])**2 for i in range(2)])**0.5
         min_dist = self.rad + ball.rad
         return dist <= min_dist
         
 
     def draw(self, screen):
+        """
+        Draws the target on the screen.
+        """
         pg.draw.circle(screen, self.color, self.coord, self.rad)
     
 class Obstacle():
     def __init__(self, coord = None, color = None, S = 20, H = 90, speed = randint(-10, 10)):
+        """Creates an obstacle.
+        :param height: height of the obstacle.
+        :param width: width of the obstacle.
+        :param coord: coordinates of the center of the obstacle.
+        :param color: color of the obstacle.
+        """
         if color == None:
             color = RED
         if coord == None:
@@ -123,12 +178,18 @@ class Obstacle():
         self.width = S
         self.height = H
         
-    def move(self):
+    def move(self): 
+        """
+        Moves the obstacle.
+        """
         self.coord[1] += self.speed
         if self.coord[1] + self.height > SCREEN_SIZE[1] or self.coord[1] < 0:
             self.speed = -self.speed
         
     def draw(self, screen):
+        """
+        Draws the obstacle on the screen.
+        """
         surf = pg.Surface((self.height, self.width))
         surf.fill(RED)
         f = pg.font.SysFont('arial', 36)
@@ -140,7 +201,13 @@ class Obstacle():
         
 
 class Manager():
+    """
+    Manages the process of the game.
+    """
     def __init__(self, n_targets = 1, n_obstacles = 1):
+        """
+        Creates a game: guns, balls, targets and a score table. Creates variables to monitor the state of the game.
+        """
         self.gun = Gun()
         self.table = Table()
         self.balls = []
@@ -152,14 +219,23 @@ class Manager():
         self.new_mission()
         
     def new_mission(self):
+        """
+        Create new targets.
+        """
         for i in range(self.n_targets):
             self.targets.append(Target(rad=randint(20, 30)))
             
     def set_obstacles(self):
+        """
+        Create new obstacles.
+        """
         for i in range(self.n_obstacles):
             self.obstacles.append(Obstacle())
         
     def process(self, events, screen):
+        """
+        Manages the game. If all the targets have been hit, creates new ones.
+        """
         done = self.handle_events(events)
         self.move()
         self.collide()
@@ -172,6 +248,9 @@ class Manager():
         return done
 
     def draw(self, screen):
+        """
+        Draws all the objects, which have to drawn on the screen.
+        """
         screen.fill(BLACK)
         SC_IMG = pg.image.load("Поставьте пожалмста полный балл.jpeg")
         screen.blit(SC_IMG, (0, 0))
@@ -187,6 +266,9 @@ class Manager():
         
         
     def move(self):
+        """
+        Moves all the objects, which have to be moved.
+        """
         for ball in self.balls:
             ball.move()
         for target in self.targets:
@@ -197,6 +279,9 @@ class Manager():
         
         
     def collide(self):
+        """
+        Checks if the balls have hit some targets.
+        """
         collisions = []
         targets_c = []
         for i, ball in enumerate(self.balls):
@@ -216,6 +301,9 @@ class Manager():
             self.targets.pop(j)
 
     def check_alive(self):
+        """
+        Checks if the balls are still moving and if the targets have not been hit yet.
+        """
         dead_balls = []
         for i, ball in enumerate(self.balls):
             if not ball.is_alive:
@@ -225,6 +313,9 @@ class Manager():
             self.balls.pop(i)
 
     def handle_events(self, events):
+        """
+        Handles the events.
+        """
         done = False
         for event in events:
             if event.type == pg.QUIT:
@@ -262,3 +353,4 @@ while not done:
     pg.display.flip()
     
 pg.quit()
+
